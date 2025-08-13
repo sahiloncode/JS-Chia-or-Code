15a16656845d18a3768f77eb032625908cf1fcae
Sahil Rajput <karanrajput4580@gmail.com>
2025-08-13 20:31:38 +0530

    Data Type Conversion And Operat0rs
    Data Type Conversion And Operators complete

---
+++ Operations/index.js
@@ -0,0 +1,20 @@
+let value= 4;
+let negValue = -value;
+console.log(negValue); // Output: -4
+
+console.log(2+5);
+console.log(2-5);
+console.log(2*5);
+console.log(2/5);
+console.log(2**5);
+console.log(2%5);
+
+let str1="Sahil"
+let str2=" Rajput"
+console.log(str1 + str2); // Output: SahilRajput 
+
+console.log("1" + 2); // Output: 12
+console.log(1 - "2"); // Output: -1
+
+//in that case which we use first number as string then it will convert the second number to string and concatenate them
+//and if we use first number as number then it will convert the second number to number and perform the operation

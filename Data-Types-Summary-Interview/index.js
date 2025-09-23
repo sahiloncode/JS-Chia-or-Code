83ea7e375d45a9608c754ac071a0e839eb29fe2c
Sahil Rajput <karanrajput4580@gmail.com>
2025-09-23 20:57:13 +0530

    Data Type Summery

---
+++ Data-Types-Summary-Interview/index.js
@@ -0,0 +1,28 @@
+// Primitive Data Types in JavaScript
+
+// It have 7  types in JavaScript:
+// 1. String
+// 2. Number
+// 3. Boolean
+// 4. Null
+// 5. Undefined
+// 6. Symbol
+// 7. BigInt
+
+
+// Non Primitive Data Types in JavaScript
+
+// It have 3 Tpes in JavaScript:
+// 1. Array
+// 2. Object
+// 3. Function
+
+//Return Type of Variables in JavaScript
+
+// 1. Number => Number
+// 2. String => String
+// 3. Boolean => Boolean
+// 4. Null => Object
+// 5. Undefined => Undefined
+// 6. Symbol => Symbol
+// 7. BigInt => BigInt
